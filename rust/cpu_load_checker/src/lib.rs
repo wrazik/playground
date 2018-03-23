57c23531bedecbd9466a764b80448fb09f364e56
wrazik <wojciech.razik@gmail.com>
2018-03-23 20:06:42 +0100

    Second, correctly working version

--- rust/cpu_load_checker/src/lib.rs
+++ rust/cpu_load_checker/src/lib.rs
@@ -1,105 +1,132 @@
 use std::error;
 use std::fs::File;
-#[cfg(not(test))] use std::io::Read;
 use std::io::BufReader;
-use std::io::prelude::*;
+use std::io::BufRead;
 use std::vec::Vec;
+use std::{thread, time};
 
 type Result<T> = std::result::Result<T, Box<error::Error>>;
 
-fn remove_trailing_newline(s: &mut String) {
-    let len_withoutcrlf = s.trim_right().len();
-    s.truncate(len_withoutcrlf);
+fn is_cpu_core_line(s: &str) -> bool {
+    let first_word = s.split_whitespace().next().unwrap();
+    first_word.starts_with("cpu") && first_word.get(3..).unwrap().parse::<i32>().is_ok()
 }
 
-trait FileOp {
-    fn get_file_content(&self, filename: String) -> Result<String>;
+#[derive(PartialEq, Eq, Debug)]
+struct ProcStats {
+    core_nb: usize,
+    user: i64,
+    nice: i64,
+    system: i64,
+    idle: i64,
+    iowait: i64,
+    irq: i64,
+    softirq: i64,
+    steal: i64,
+    guest: i64,
+    guest_nice: i64,
 }
 
-struct Fs;
+impl ProcStats {
+    fn get_idle(&self) -> i64 {
+        self.idle + self.iowait
+    }
 
-#[cfg(not(test))]
-impl FileOp for Fs {
-    fn get_file_content(&self, filename: String) -> Result<String> {
-        let mut file = File::open(filename)?;
-        let mut content = String::new();
-        file.read_to_string(&mut content)?;
-        Ok(content)
+    fn get_non_idle(&self) -> i64 {
+        self.user + self.nice + self.system + self.irq + self.softirq + self.steal
+    }
+
+    fn get_total(&self) -> i64 {
+        self.get_idle() + self.get_non_idle()
     }
 }
 
-pub fn read_current_freq() -> Result<Vec<(usize, f64)>> {
-    let file = File::open("/proc/cpuinfo")?;
-    let file = BufReader::new(file);
-    let mut frequencies: Vec<(usize, f64)> = Vec::new();
-    let mut current_core_nb = 0;
-
-    for line in file.lines().filter_map(|result| result.ok()) {
-        if line.starts_with("processor") {
-            let nb = line.split(" ").nth(1).unwrap();
-            current_core_nb = nb.parse::<usize>()?;
-        }
-        else if line.starts_with("cpu MHz") {
-            let nb = line.split(" ").nth(2).unwrap();
-            let current_freq = nb.parse::<f64>()?;
-            frequencies.push((current_core_nb, current_freq));
-        }
-    }
-    Ok(frequencies)
+fn get_core_nb(word: &str) -> usize {
+    word.get(3..).unwrap().parse::<usize>().unwrap()
 }
 
-pub fn read_max_freq(core_nb: usize) -> Result<f64>{
-    let filename = format!("/sys/devices/system/cpu/cpu{}/cpufreq/cpuinfo_max_freq", core_nb);
-    let mut content = Fs.get_file_content(filename)?;
-    remove_trailing_newline(&mut content);
-    Ok(content.parse()?)
+fn create_single_proc_stats(line: &str) -> ProcStats {
+    let mut word_it = line.split_whitespace();
+    let core_nb = get_core_nb(&word_it.next().unwrap());
+    let as_nums: Vec<_> = word_it.map(|word| {
+        word.parse::<i64>().unwrap()
+    }).collect();
+
+    ProcStats {
+        core_nb: core_nb,
+        user: as_nums[0],
+        nice: as_nums[1],
+        system: as_nums[2],
+        idle: as_nums[3],
+        iowait: as_nums[4],
+        irq: as_nums[5],
+        softirq: as_nums[6],
+        steal: as_nums[7],
+        guest: as_nums[8],
+        guest_nice: as_nums[9],
+    }
+}
+
+fn read_proc_stats() -> Result<Vec<ProcStats>> {
+    let proc_stat_file = File::open("/proc/stat")?;
+    let proc_stat_lines = BufReader::new(&proc_stat_file);
+    let result: Vec<_> = proc_stat_lines.lines()
+        .map(|line| line.unwrap())
+        .filter(|line| is_cpu_core_line(&line))
+        .map(|line| create_single_proc_stats(&line))
+        .collect();
+    Ok(result)
+}
+
+fn get_cpu_freqs(prev: &Vec<ProcStats>, curr: &Vec<ProcStats>) -> Result<Vec<(usize, f64)>> {
+    assert_eq!(prev.len(), curr.len());
+    let mut result  = Vec::new();
+
+    for (previous, current) in prev.iter().zip(curr.iter()) {
+        assert_eq!(previous.core_nb, current.core_nb);
+        let totald = (current.get_total() - previous.get_total()) as f64;
+        let idled = (current.get_idle() - previous.get_idle()) as f64;
+        result.push((previous.core_nb, (totald-idled)/totald*100.0));
+    }
+    Ok(result)
 }
 
 pub fn get_curr_freqs() -> Result<Vec<(usize, f64)>> {
-    let current_freqs = read_current_freq()?;
-    let in_percent = current_freqs.iter().map(|result| {
-        let core_nb = result.0;
-        let max_freq = read_max_freq(core_nb).unwrap();
-        let current_in_percent = result.1 / max_freq * 100.0;
-        (core_nb, current_in_percent)
-    }).collect::<Vec<_>>();
-
-    Ok(in_percent)
+    let first_measurement = read_proc_stats()?;
+    thread::sleep(time::Duration::from_secs(1));
+    let second_measurement = read_proc_stats()?;
+    get_cpu_freqs(&first_measurement, &second_measurement)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[cfg(test)]
-    impl FileOp for Fs {
-        fn get_file_content(&self, _filename: String) -> Result<String> {
-            Ok(String::from(Fs::CONTENT))
-        }
+    #[test]
+    fn is_cpu_core_line_test() {
+        assert_eq!(false, is_cpu_core_line("fake_line"));
+        assert_eq!(false, is_cpu_core_line("cpufake"));
+        assert_eq!(false, is_cpu_core_line("cpu 12 31 231"));
+        assert_eq!(true, is_cpu_core_line("cpu0 2 3 1 24 4 12 4"));
     }
 
     #[test]
-    fn remove_trailing_newline_test() {
-        let test_str = "Test";
-        let mut windows_specific = format!("{}\r\n", test_str);
-        let mut linux_specific = format!("{}\n", test_str);
-        let mut no_endline = format!("{}", test_str);
-        remove_trailing_newline(&mut windows_specific);
-        remove_trailing_newline(&mut linux_specific);
-        remove_trailing_newline(&mut no_endline);
-        assert_eq!(windows_specific, test_str);
-        assert_eq!(linux_specific, test_str);
-        assert_eq!(no_endline, test_str);
-    }
+    fn create_single_proc_stats_test() {
+        let expected = ProcStats {
+            core_nb: 3,
+            user: 24082,
+            nice: 250,
+            system: 3356,
+            idle: 65772,
+            iowait: 481,
+            irq: 488,
+            softirq: 199,
+            steal: 0,
+            guest: 0,
+            guest_nice: 0,
+        };
 
-    #[test]
-    fn read_max_freq_test() {
-        const FREQ: f64 = 102303.0;
-
-        impl Fs {
-             const CONTENT: &'static str = "102303\n";
-        }
-
-        assert_eq!(read_max_freq(0).unwrap(), FREQ);
+        let actual = create_single_proc_stats("cpu3 24082 250 3356 65772 481 488 199 0 0 0");
+        assert_eq!( actual, expected);
     }
 }
--- rust/cpu_load_checker/src/main.rs
+++ rust/cpu_load_checker/src/main.rs
@@ -4,8 +4,8 @@ use cpu_load::get_curr_freqs;
 fn main() {
     match get_curr_freqs() {
         Ok(freq_vec) => {
-            for (core_nb, cpu_freq) in freq_vec {
-                print!("| Cpu{}: {:.2}% ", core_nb, cpu_freq);
+            for (core_nb, val) in freq_vec {
+                print!("|\tCPU{}: {:.2}%\t", core_nb, val);
             }
             println!("|");
         }
