cb93e26d5575c0af91f1a35c1f33c04dbb6f0a70
wrazik <wojciech.razik@gmail.com>
2018-10-11 07:22:54 +0200

    Iteration 2

--- rust/race_ml/src/main.rs
+++ rust/race_ml/src/main.rs
@@ -16,15 +16,37 @@ pub struct Position {
 
 pub struct Sprite {
     position: Position,
+    velocity: [f64; 2],
     speed: f64
 }
 
 impl Sprite {
     fn up(&mut self) {
-        self.position.y -= 1.0*self.speed;
+        self.velocity = [0., -self.speed]
     }
     fn down(&mut self) {
-        self.position.y += 1.0*self.speed;
+        self.velocity = [0., self.speed]
+    }
+    fn stop(&mut self) {
+        self.velocity = [0., 0.]
+    }
+
+    fn update(&mut self) {
+        self.position.x += self.velocity[0];
+
+        if (self.position.x < 0.) {
+            self.position.x = 0.
+        }
+        else if (self.position.x > 800.) {
+            self.position.x = 800.;
+        }
+        self.position.y += self.velocity[1];
+        if (self.position.y < 0.) {
+            self.position.y = 0.
+        }
+        else if (self.position.y >  550.) {
+            self.position.y = 550.;
+        }
     }
 }
 
@@ -32,7 +54,36 @@ pub struct Pad {
     sprite: Sprite
 }
 
+pub struct Ball {
+    sprite: Sprite
+}
 
+impl Ball {
+    fn draw(&mut self, gl: &mut GlGraphics, args: &RenderArgs) {
+        use graphics::*;
+
+        let (x, y) = (self.sprite.position.x,
+                      self.sprite.position.y);
+
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform.trans(x, y);
+
+            rectangle(Pad::WHITE, Pad::PAD, transform, gl);
+        });
+    }
+    
+    fn bounce_x() {
+       self.position.x = -self.position.x;
+    }
+
+    fn bounce_y() {
+       self.position.y = self.position.y;
+    }
+
+    fn update(&mut self) {
+        self.sprite.update();
+    }
+}
 
 impl Pad {
     const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
@@ -59,6 +110,14 @@ impl Pad {
     fn down(&mut self) {
         self.sprite.down();
     }
+
+    fn stop(&mut self) {
+        self.sprite.stop();
+    }
+
+    fn update(&mut self) {
+        self.sprite.update();
+    }
 }
 
 pub struct App {
@@ -81,6 +140,35 @@ impl App {
         self.left_pad.draw(&mut self.gl, args);
         self.right_pad.draw(&mut self.gl, args);
     }
+
+    fn update(&mut self, args: &UpdateArgs) {
+        self.left_pad.update();
+        self.right_pad.update();
+    }
+
+    fn release(&mut self, key: &Button) {
+        match key {
+            Button::Keyboard(k) => {
+                match k {
+                    Key::W =>  {
+                        self.left_pad.stop();
+                    },
+                    Key::S => { 
+                        self.left_pad.stop();
+                    },
+                    Key::Up =>  {
+                        self.right_pad.stop();
+                    },
+                    Key::Down => { 
+                        self.right_pad.stop();
+                    },
+                    _ => {} 
+                }
+            }
+            _ => {}
+        }
+    }
+
 
     fn key_pressed(&mut self, key: &Button) {
         // Rotate 2 radians per second.
@@ -129,7 +217,8 @@ fn main() {
                     x: 10.0, 
                     y: 280.0
                 },
-                speed: 30.0
+                velocity: [0., 0.],
+                speed: 3.0
             }
         },
         right_pad: Pad {
@@ -138,7 +227,8 @@ fn main() {
                     x: 775.0, 
                     y: 280.0
                 },
-                speed: 30.0
+                velocity: [0., 0.],
+                speed: 3.0
             }
         },
     };
@@ -148,8 +238,14 @@ fn main() {
         if let Some(k) = e.press_args() {
             app.key_pressed(&k);
         }
+        if let Some(rel_args) = e.release_args() {
+            app.release(&rel_args)
+        }
         if let Some(r) = e.render_args() {
             app.render(&r);
+        }
+        if let Some(u) = e.update_args() {
+            app.update(&u);
         }
     }
 }
